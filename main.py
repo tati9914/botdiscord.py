1e1970ffdfd36a0d50aad7193a2d1121c3d4055d
tati9914 <tatikononovas@gmail.com>
2025-03-04 15:00:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -47,4 +47,17 @@ async def broma(ctx):
     ]
     chiste = random.choice(chistes)
     await ctx.send(f"Chiste: {chiste}")
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
     
